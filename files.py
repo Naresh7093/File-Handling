279ddcfc0b2d76f0308e6805c9c9b71a164d4507
Naresh Tallapaka <111639266+Naresh7093@users.noreply.github.com>
2025-07-02 15:24:12 +0530

    Update files.py

--- files.py
+++ files.py
@@ -4,13 +4,10 @@ import pandas as pd
 st.subheader("Loading the CSV file")
 df = st.file_uploader("Upload the CSV file : ",type = ['csv','xlsx'])
 
-df = pd.read_csv('C:/Users/nares/Desktop/streamlit/Tweets.csv')
+df = pd.read_csv('Tweets.csv')
 
 if df is not None:
     st.table(df.head())
-
-st.subheader("Dealing with images")
-st.image("C:/Users/nares/Desktop/streamlit/presentation.png")
 
 st.subheader("Dealing with images while uploading")
 img_file = st.file_uploader("Upload the Image file : ",type=['png','jpeg'])
